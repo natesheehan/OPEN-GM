a0edc90a1afbebab406c267c2a94c5aef2b91548
natesheehan <nathanaelsheehan@gmail.com>
2022-11-10 13:21:45 +0000

    update temporal  plots per Federico and Sabina comments

--- code/plot-temporal-submissions.r
+++ code/plot-temporal-submissions.r
@@ -16,6 +16,66 @@
 ##
 ##
 ## ---------------------------
+
+theme_temporal = function() {
+    theme(
+      legend.position = "bottom",
+      legend.direction = "horizontal",
+      legend.title = element_text(
+        colour = textcol,
+        face = "italic",
+        size = 14
+      ),
+      legend.margin = margin(grid::unit(0, "cm")),
+      legend.text = element_text(
+        colour = textcol,
+        size = 14,
+        face = "bold"
+      ),
+      legend.key.height = grid::unit(1.4, "cm"),
+      legend.key.width = grid::unit(0.8, "cm"),
+      axis.text.x = element_text(
+        size = 6,
+        angle = 90,
+        vjust = 0.5,
+        hjust = 1,
+        color = textcol
+      ),
+      axis.text.y = element_text(
+        vjust = 0.2,
+        colour = textcol,
+        size = 14
+      ),
+      axis.ticks = element_line(size = 0.4),
+      plot.caption = element_text(colour = textcol, size = 10),
+      axis.title = element_text(
+        size = 12,
+        face = "bold",
+        colour = textcol,
+        hjust = 0.1
+      ),
+      panel.border = element_blank(),
+      panel.background = element_rect(
+        fill = "transparent",
+        colour = "transparent",
+        size = 0.5,
+        linetype = "solid"
+      ),
+      panel.grid.major = element_blank(),
+      legend.key = element_rect(fill = "transparent", colour = "transparent"),
+      plot.background = element_rect(fill = "gray12"),
+      legend.background = element_rect(fill = "transparent"),
+      plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
+      plot.title = element_text(
+        colour = textcol,
+        size = 18,
+        face = "bold",
+        vjust = 0.9
+      )
+    )
+}
+
+# Load data & create plot dataframes ---------------------------------------------------------------
 gisaid = readRDS("data/gisaid.RDS")
 embl = readRDS("data/embl.RDS")
 
@@ -44,13 +104,15 @@ cd19dp_temporal_subs_av = embl |>
   summarise(av_cd19dp = mean(C19DP.weekly.submissions)) |>
   arrange(wy)
 
-
-
 temporal_sub_all = right_join(gisaid_temporal_subs, cd19dp_temporal_subs)
 temporal_sub_all_av = right_join(gisaid_temporal_subs_av, cd19dp_temporal_subs_av)
 
+
+
+# Plot total --------------------------------------------------------------
 textcol = "white"
 
+# SUM
 ggplot(data = temporal_sub_all) +
   geom_line(aes(
     x = wy,
@@ -64,18 +126,11 @@ ggplot(data = temporal_sub_all) +
     group = 1,
     color = 'CD19DP Monthly Total'
   )) +
-  # annotate("text", x = 7, y = 60000, label = "WHO Declares\n Pandemic") +
-  # annotate("text", x = 17, y = 6000, label = "AstraZeneca's \nVaccine Authorised") +
-  # annotate("text", x = 18, y = 600000, label = "EBI Open letter") +
-  # annotate("text", x = 27, y = 700000, label = "Global Covid-19 Deaths \nPass Five Million") +
   scale_colour_manual(
     "",
-    breaks = c(
-      "GISAID Monthly Total",
-      "CD19DP Monthly Total",
-      "Genebank Monthly Total"
-    ),
-    values = c("green", "blue")
+    breaks = c("GISAID Monthly Total",
+               "CD19DP Monthly Total"),
+    values = c("seagreen1", "cyan")
   ) +
   guides(shape = guide_legend(order = 2), col = guide_legend(order = 1)) +
   labs(
@@ -83,64 +138,7 @@ ggplot(data = temporal_sub_all) +
     y = "Sequence Submissions",
     title = "Monthly Total SARS-CoV-2 Sequence Submissions",
     caption = "\nMonthly totals of global SARS-CoV-2 cases sequenced and shared on the GISAID \nand Covid-19 Data Platform database between December 2019 and October 2022\n\n\nGISAID Metadata: https://www.epicov.org/\nCovid-19 Data Platform Metadata: https://www.ebi.ac.uk/ena/portal/api/ "
-  ) +
-  theme(legend.position = "none",
-        plot.title = element_text(size = 11)) +
-  theme(
-    legend.position = "bottom",
-    legend.direction = "horizontal",
-    legend.title = element_text(
-      colour = textcol,
-      face = "italic",
-      size = 14
-    ),
-    legend.margin = margin(grid::unit(0, "cm")),
-    legend.text = element_text(
-      colour = textcol,
-      size = 14,
-      face = "bold"
-    ),
-    legend.key.height = grid::unit(0.8, "cm"),
-    legend.key.width = grid::unit(0.2, "cm"),
-    axis.text.x = element_text(
-      size = 6,
-      angle = 90,
-      vjust = 0.5,
-      hjust = 1,
-      color = textcol
-    ),
-    axis.text.y = element_text(
-      vjust = 0.2,
-      colour = textcol,
-      size = 14
-    ),
-    axis.ticks = element_line(size = 0.4),
-    plot.caption = element_text(colour = textcol, size = 10),
-    axis.title = element_text(
-      size = 12,
-      face = "bold",
-      colour = textcol,
-      hjust = 0.1
-    ),
-    panel.border = element_blank(),
-    panel.background = element_rect(
-      fill = "transparent",
-      colour = "transparent",
-      size = 0.5,
-      linetype = "solid"
-    ),
-    panel.grid.major = element_blank(),
-    legend.key = element_rect(fill = "transparent", colour = "transparent"),
-    plot.background = element_rect(fill = "gray12"),
-    legend.background = element_rect(fill = "transparent"),
-    plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
-    plot.title = element_text(
-      colour = textcol,
-      size = 18,
-      face = "bold",
-      vjust = 0.9
-    )
-  )
+  ) + theme_temporal()
 
 ggsave(
   paste0("plots/temporal_sum.png"),
@@ -150,6 +148,7 @@ ggsave(
   limitsize = FALSE
 )
 
+# MEAN
 ggplot(data = temporal_sub_all_av) +
   geom_line(aes(
     x = wy,
@@ -163,85 +162,23 @@ ggplot(data = temporal_sub_all_av) +
     group = 1,
     color = 'CD19DP Monthly Total'
   )) +
-  # annotate("text", x = 7, y = 60000, label = "WHO Declares\n Pandemic") +
-  # annotate("text", x = 17, y = 6000, label = "AstraZeneca's \nVaccine Authorised") +
-  # annotate("text", x = 18, y = 600000, label = "EBI Open letter") +
-  # annotate("text", x = 27, y = 700000, label = "Global Covid-19 Deaths \nPass Five Million") +
   scale_colour_manual(
     "",
-    breaks = c(
-      "GISAID Monthly Total",
-      "CD19DP Monthly Total",
-      "Genebank Monthly Total"
-    ),
-    values = c("green", "blue")
+    breaks = c("GISAID Monthly Total",
+               "CD19DP Monthly Total"),
+    values = c("seagreen1", "cyan")
   ) +
   guides(shape = guide_legend(order = 2), col = guide_legend(order = 1)) +
   labs(
     x = "Date",
-    y = "Sequence Submissions",
+    y = "Sequence Submissions (Logged)",
     title = "Monthly Mean SARS-CoV-2 Sequence Submissions",
     caption = "\nMonthly mean of global SARS-CoV-2 cases sequenced and shared on the GISAID \nand Covid-19 Data Platform database between December 2019 and October 2022\n\n\nGISAID Metadata: https://www.epicov.org/\nCovid-19 Data Platform Metadata: https://www.ebi.ac.uk/ena/portal/api/ "
-  ) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + theme(legend.position =
-                                                                           "none",
-                                                                         plot.title = element_text(size = 11)) +
-  theme(
-    legend.position = "bottom",
-    legend.direction = "horizontal",
-    legend.title = element_text(
-      colour = textcol,
-      face = "italic",
-      size = 14
-    ),
-    legend.margin = margin(grid::unit(0, "cm")),
-    legend.text = element_text(
-      colour = textcol,
-      size = 14,
-      face = "bold"
-    ),
-    legend.key.height = grid::unit(0.8, "cm"),
-    legend.key.width = grid::unit(0.2, "cm"),
-    axis.text.x = element_text(
-      size = 6,
-      angle = 90,
-      vjust = 0.5,
-      hjust = 1,
-      color = textcol
-    ),
-    axis.text.y = element_text(
-      vjust = 0.2,
-      colour = textcol,
-      size = 14
-    ),
-    axis.ticks = element_line(size = 0.4),
-    plot.caption = element_text(colour = textcol, size = 10),
-    axis.title = element_text(
-      size = 12,
-      face = "bold",
-      colour = textcol,
-      hjust = 0.1
-    ),
-    panel.border = element_blank(),
-    panel.background = element_rect(
-      fill = "transparent",
-      colour = "transparent",
-      size = 0.5,
-      linetype = "solid"
-    ),
-    panel.grid.major = element_blank(),
-    legend.key = element_rect(fill = "transparent", colour = "transparent"),
-    plot.background = element_rect(fill = "gray12"),
-    legend.background = element_rect(fill = "gray12"),
-    plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
-    plot.title = element_text(
-      colour = textcol,
-      size = 18,
-      face = "bold",
-      vjust = 0.9
-    )
-  )
+  ) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  theme_temporal()
+
 ggsave(
-  paste0(embl$"plots/temporal_average.png"),
+  paste0("plots/temporal_average.png"),
   dpi = 320,
   width = 18,
   height = 12,
@@ -249,26 +186,9 @@ ggsave(
 )
 
 
-###### LOG plot
-## ---------------------------
-##
-## Script name: plot-temporal-submissions.r
-##
-## Purpose of script: plot temporal submissions
-##
-## Author: Nathanael Sheehan
-##
-## Date Created: 2022-11-01
-##
-## Copyleft (c) Nathanael Sheehan, 2022
-##
-## ---------------------------
-##
-## Notes:
-##
-##
-## ---------------------------
+# Plot log ----------------------------------------------------------------
 
+# SUM
 ggplot(data = temporal_sub_all) +
   geom_line(aes(
     x = wy,
@@ -282,10 +202,6 @@ ggplot(data = temporal_sub_all) +
     group = 1,
     color = 'CD19DP Monthly Total'
   )) +
-  # annotate("text", x = 7, y = 60000, label = "WHO Declares\n Pandemic") +
-  # annotate("text", x = 17, y = 6000, label = "AstraZeneca's \nVaccine Authorised") +
-  # annotate("text", x = 18, y = 600000, label = "EBI Open letter") +
-  # annotate("text", x = 27, y = 700000, label = "Global Covid-19 Deaths \nPass Five Million") +
   scale_colour_manual(
     "",
     breaks = c(
@@ -293,72 +209,18 @@ ggplot(data = temporal_sub_all) +
       "CD19DP Monthly Total",
       "Genebank Monthly Total"
     ),
-    values = c("green", "blue")
+    values = c("seagreen1", "cyan")
   ) +
   guides(shape = guide_legend(order = 2), col = guide_legend(order = 1)) +
   labs(
     x = "Date",
-    y = "Sequence Submissions",
+    y = "Sequence Submissions (Logged)",
     title = "Monthly Total SARS-CoV-2 Sequence Submissions",
     caption = "\nMonthly totals (log) of global SARS-CoV-2 cases sequenced and shared on the GISAID and Covid-19 Data Platform database between December 2019 and October 2022\n\n\nGISAID Metadata: https://www.epicov.org/\nCovid-19 Data Platform Metadata: https://www.ebi.ac.uk/ena/portal/api/ "
   ) +
   theme(legend.position = "none",
         plot.title = element_text(size = 11)) +
-  theme(
-    legend.position = "bottom",
-    legend.direction = "horizontal",
-    legend.title = element_text(
-      colour = textcol,
-      face = "italic",
-      size = 14
-    ),
-    legend.margin = margin(grid::unit(0, "cm")),
-    legend.text = element_text(
-      colour = textcol,
-      size = 14,
-      face = "bold"
-    ),
-    legend.key.height = grid::unit(0.8, "cm"),
-    legend.key.width = grid::unit(0.2, "cm"),
-    axis.text.x = element_text(
-      size = 6,
-      angle = 90,
-      vjust = 0.5,
-      hjust = 1,
-      color = textcol
-    ),
-    axis.text.y = element_text(
-      vjust = 0.2,
-      colour = textcol,
-      size = 14
-    ),
-    axis.ticks = element_line(size = 0.4),
-    plot.caption = element_text(colour = textcol, size = 10),
-    axis.title = element_text(
-      size = 12,
-      face = "bold",
-      colour = textcol,
-      hjust = 0.1
-    ),
-    panel.border = element_blank(),
-    panel.background = element_rect(
-      fill = "transparent",
-      colour = "transparent",
-      size = 0.5,
-      linetype = "solid"
-    ),
-    panel.grid.major = element_blank(),
-    legend.key = element_rect(fill = "transparent", colour = "transparent"),
-    plot.background = element_rect(fill = "gray12"),
-    legend.background = element_rect(fill = "transparent"),
-    plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
-    plot.title = element_text(
-      colour = textcol,
-      size = 18,
-      face = "bold",
-      vjust = 0.9
-    )
-  ) +   scale_y_continuous(trans = 'log2')
+  theme_temporal()+   scale_y_continuous(trans = 'log2')
 
 ggsave(
   paste0("plots/temporal_sum_log.png"),
@@ -367,6 +229,8 @@ ggsave(
   height = 12,
   limitsize = FALSE
 )
+
+# MEAN
 
 ggplot(data = temporal_sub_all_av) +
   geom_line(aes(
@@ -381,18 +245,13 @@ ggplot(data = temporal_sub_all_av) +
     group = 1,
     color = 'CD19DP Monthly Total'
   )) +
-  # annotate("text", x = 7, y = 60000, label = "WHO Declares\n Pandemic") +
-  # annotate("text", x = 17, y = 6000, label = "AstraZeneca's \nVaccine Authorised") +
-  # annotate("text", x = 18, y = 600000, label = "EBI Open letter") +
-  # annotate("text", x = 27, y = 700000, label = "Global Covid-19 Deaths \nPass Five Million") +
   scale_colour_manual(
     "",
     breaks = c(
       "GISAID Monthly Total",
-      "CD19DP Monthly Total",
-      "Genebank Monthly Total"
+      "CD19DP Monthly Total"
     ),
-    values = c("green", "blue")
+    values = c("seagreen1", "cyan")
   ) +
   guides(shape = guide_legend(order = 2), col = guide_legend(order = 1)) +
   labs(
@@ -400,64 +259,8 @@ ggplot(data = temporal_sub_all_av) +
     y = "Sequence Submissions",
     title = "Monthly Mean SARS-CoV-2 Sequence Submissions",
     caption = "\nMonthly mean (log) of global SARS-CoV-2 cases sequenced and shared on the GISAID and Covid-19 Data Platform database between December 2019 and October 2022\n\n\nGISAID Metadata: https://www.epicov.org/\nCovid-19 Data Platform Metadata: https://www.ebi.ac.uk/ena/portal/api/ "
-  ) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + theme(legend.position =
-                                                                           "none",
-                                                                         plot.title = element_text(size = 11)) +
-  theme(
-    legend.position = "bottom",
-    legend.direction = "horizontal",
-    legend.title = element_text(
-      colour = textcol,
-      face = "italic",
-      size = 14
-    ),
-    legend.margin = margin(grid::unit(0, "cm")),
-    legend.text = element_text(
-      colour = textcol,
-      size = 14,
-      face = "bold"
-    ),
-    legend.key.height = grid::unit(0.8, "cm"),
-    legend.key.width = grid::unit(0.2, "cm"),
-    axis.text.x = element_text(
-      size = 6,
-      angle = 90,
-      vjust = 0.5,
-      hjust = 1,
-      color = textcol
-    ),
-    axis.text.y = element_text(
-      vjust = 0.2,
-      colour = textcol,
-      size = 14
-    ),
-    axis.ticks = element_line(size = 0.4),
-    plot.caption = element_text(colour = textcol, size = 10),
-    axis.title = element_text(
-      size = 12,
-      face = "bold",
-      colour = textcol,
-      hjust = 0.1
-    ),
-    panel.border = element_blank(),
-    panel.background = element_rect(
-      fill = "transparent",
-      colour = "transparent",
-      size = 0.5,
-      linetype = "solid"
-    ),
-    panel.grid.major = element_blank(),
-    legend.key = element_rect(fill = "transparent", colour = "transparent"),
-    plot.background = element_rect(fill = "gray12"),
-    legend.background = element_rect(fill = "gray12"),
-    plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
-    plot.title = element_text(
-      colour = textcol,
-      size = 18,
-      face = "bold",
-      vjust = 0.9
-    )
-  ) +  scale_y_continuous(trans = 'log2')
+  ) + theme_temporal() + scale_y_continuous(trans = 'log2')
+
 ggsave(
   paste0("plots/temporal_average_log.png"),
   dpi = 320,
