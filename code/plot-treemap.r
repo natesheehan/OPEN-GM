05346c283b4b23c9ac962e470bb903d597395ca7
natesheehan <nathanaelsheehan@gmail.com>
2022-11-11 13:01:24 +0000

    update treemap plot per sabinas comments

--- code/plot-treemap.r
+++ code/plot-treemap.r
@@ -18,33 +18,8 @@
 ## ---------------------------
 library(ggpubr)
 
-
-tree_df = main_df |> dplyr::filter(wy == "22/39")
-textcol = "grey40"
-a = ggplot(
-  tree_df,
-  aes(
-    area = GISAID.total.submissions,
-    fill = GISAID.total.submissions,
-    label = country,
-    subgroup = continent
-  )
-) +
-  geom_treemap() +
-  geom_treemap_subgroup_border(colour = "white", size = 5) +
-  geom_treemap_subgroup_text(
-    place = "centre",
-    grow = TRUE,
-    alpha = 0.25,
-    colour = "black",
-    fontface = "italic"
-  ) +
-  geom_treemap_text(
-    colour = "white",
-    place = "centre",
-    size = 15,
-    grow = TRUE
-  ) +  theme(
+theme_tree = function(){
+  theme(
     legend.position = "none",
     legend.direction = "horizontal",
     legend.title = element_text(
@@ -73,7 +48,7 @@ a = ggplot(
     axis.ticks = element_line(size = 0.4),
     plot.caption = element_text(colour = textcol, size = 10),
     axis.title = element_text(
-      size = 12,
+      size = 14,
       face = "bold",
       colour = textcol,
       hjust = 0.1
@@ -92,12 +67,41 @@ a = ggplot(
     plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
     plot.title = element_text(
       colour = textcol,
-      size = 18,
+      size = 26,
       face = "bold",
       vjust = 0.9
     )
-  ) +   labs(title = "\nGlobal SARS-CoV-2 cases sequenced and shared with the GISAID \n\n",
-             caption = "\nGISAID Metadata: https://www.epicov.org/")
+  )
+}
+
+
+tree_df = main_df |> dplyr::filter(wy == "22/39")
+textcol = "yellow"
+a = ggplot(
+  tree_df,
+  aes(
+    area = GISAID.total.submissions,
+    fill = GISAID.total.submissions,
+    label = country,
+    subgroup = continent
+  )
+) +
+  geom_treemap() +
+  geom_treemap_subgroup_border(colour = "white", size = 5) +
+  geom_treemap_subgroup_text(
+    place = "centre",
+    grow = TRUE,
+    alpha = 0.25,
+    colour = "yellow",
+    fontface = "italic"
+  ) +
+  geom_treemap_text(
+    colour = "white",
+    place = "centre",
+    size = 15,
+    grow = TRUE
+  ) +   labs(title = "\nGlobal SARS-CoV-2 cases sequenced and shared with \nGISAID \n\n",
+             caption = "\nGISAID Metadata: https://www.epicov.org/") + theme_tree()
 
 b = ggplot(
   tree_df,
@@ -114,7 +118,7 @@ b = ggplot(
     place = "centre",
     grow = TRUE,
     alpha = 0.25,
-    colour = "black",
+    colour = "yellow",
     fontface = "italic"
   ) +
   geom_treemap_text(
@@ -122,60 +126,9 @@ b = ggplot(
     place = "centre",
     size = 15,
     grow = TRUE
-  ) +  theme(
-    legend.position = "none",
-    legend.direction = "horizontal",
-    legend.title = element_text(
-      colour = textcol,
-      face = "italic",
-      size = 14
-    ),
-    legend.margin = margin(grid::unit(0, "cm")),
-    legend.text = element_text(
-      colour = textcol,
-      size = 14,
-      face = "bold"
-    ),
-    axis.text.x = element_text(
-      size = 6,
-      angle = 90,
-      vjust = 0.5,
-      hjust = 1,
-      color = textcol
-    ),
-    axis.text.y = element_text(
-      vjust = 0.2,
-      colour = textcol,
-      size = 14
-    ),
-    axis.ticks = element_line(size = 0.4),
-    plot.caption = element_text(colour = textcol, size = 10),
-    axis.title = element_text(
-      size = 12,
-      face = "bold",
-      colour = textcol,
-      hjust = 0.1
-    ),
-    panel.border = element_blank(),
-    panel.background = element_rect(
-      fill = "transparent",
-      colour = "transparent",
-      size = 0.5,
-      linetype = "solid"
-    ),
-    panel.grid.major = element_blank(),
-    legend.key = element_rect(fill = "transparent", colour = "transparent"),
-    plot.background = element_rect(fill = "gray12"),
-    legend.background = element_rect(fill = "gray12"),
-    plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
-    plot.title = element_text(
-      colour = textcol,
-      size = 18,
-      face = "bold",
-      vjust = 0.9
-    )
-  ) +   labs(title = "\nGlobal SARS-CoV-2 cases sequenced and shared with the Covid-19 Data Portal \n\n",
-             caption = "\nThe Covid-19 Data Portal Metadata: https://www.covid19dataportal.org/")
+  ) +  labs(title = "\nGlobal SARS-CoV-2 cases sequenced and shared with \nthe Covid-19 Data Portal \n\n",
+             caption = "\nThe Covid-19 Data Portal Metadata: https://www.covid19dataportal.org/") +
+  theme_tree()
 
 ggarrange(a, b)
 
