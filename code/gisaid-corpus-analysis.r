f6c68242b2ad553df5cb86d060fd9859d79482b5
natesheehan <nathanaelsheehan@gmail.com>
2022-11-14 21:34:09 +0000

    update script indent

--- code/gisaid-corpus-analysis.r
+++ code/gisaid-corpus-analysis.r
@@ -27,7 +27,7 @@ M = bibliometrix::convert2df(file = file,
                              format = "csv") |>
   dplyr::filter(PY != is.na(PY)) |>
   dplyr::filter(AB != is.na(AB)) |>
-  dplyr::mutate(Year = substr(PY,1,4)) |>
+  dplyr::mutate(Year = substr(PY, 1, 4)) |>
   dplyr::filter(Year > 2019) |>
   dplyr::select(-c(Year))
 # Conduct a biblio analysis of dataframe using the bibliometrix package
@@ -41,23 +41,31 @@ SCP = S$MostProdCountries$SCP
 MCP = S$MostProdCountries$MCP
 Articles = S$MostProdCountries$Articles
 
-data = as.data.frame(cbind(Country,SCP,MCP,Articles)) %>%
-  arrange(desc(Articles) ) %>%
-  pivot_longer(c(SCP,MCP)) %>%
+data = as.data.frame(cbind(Country, SCP, MCP, Articles)) %>%
+  arrange(desc(Articles)) %>%
+  pivot_longer(c(SCP, MCP)) %>%
   mutate(value = as.numeric(value)) %>%
   mutate(Articles = as.numeric(Articles)) %>%
   rename(collaboration = name)
 
-ggplot(data[1:100,], aes(fill=collaboration, y=value, x=reorder(Country,Articles))) +
-  geom_bar(position="stack", stat="identity") +
-  labs(title = "Studies citing GISAID",caption  = "Biblographic data was accessed by querying 'GISAID'' the dimensions.ai API between January first 2020 and October 1st 2021\nSCP: Single Country Publication. MCP: Multi Country Publication") +
+ggplot(data[1:100, ], aes(
+  fill = collaboration,
+  y = value,
+  x = reorder(Country, Articles)
+)) +
+  geom_bar(position = "stack", stat = "identity") +
+  labs(title = "Studies citing GISAID", caption  = "Biblographic data was accessed by querying 'GISAID'' the dimensions.ai API between January first 2020 and October 1st 2021\nSCP: Single Country Publication. MCP: Multi Country Publication") +
   xlab("Country") +
   ylab("No. Documents") +
   coord_flip() + theme_landscape()
 
 # Co-word Analysis through Keyword co-occurrences
 
-NetMatrix <- biblioNetwork(M, analysis = "co-occurrences", network = "keywords", sep = ";")
+NetMatrix <-
+  biblioNetwork(M,
+                analysis = "co-occurrences",
+                network = "keywords",
+                sep = ";")
 summary(networkStat(NetMatrix))
 # Main statistics about the network
 #
@@ -68,22 +76,51 @@ summary(networkStat(NetMatrix))
 # Degree Centralization                 0.918
 # Average path length                   2.012
 
-net=bibliometrix::networkPlot(NetMatrix, normalize="association", n = 50,
-                Title = "Keyword Co-occurrences \nGISAID", type = "fruchterman",
-                size.cex=TRUE, size=20, remove.multiple=F, edgesize = 10,
-                labelsize=5,label.cex=TRUE,label.n=30,edges.min=2, label.color = FALSE)
+net = bibliometrix::networkPlot(
+  NetMatrix,
+  normalize = "association",
+  n = 50,
+  Title = "Keyword Co-occurrences \nGISAID",
+  type = "fruchterman",
+  size.cex = TRUE,
+  size = 20,
+  remove.multiple = F,
+  edgesize = 10,
+  labelsize = 5,
+  label.cex = TRUE,
+  label.n = 30,
+  edges.min = 2,
+  label.color = FALSE
+)
 
 net2VOSviewer(net, vos.path = "VOSviewer/")
 
 # Author collaboration network
-NetMatrix <- biblioNetwork(M, analysis = "collaboration",  network = "authors", sep = ";")
+NetMatrix <-
+  biblioNetwork(M,
+                analysis = "collaboration",
+                network = "authors",
+                sep = ";")
 summary(networkStat(NetMatrix))
 
-net=networkPlot(NetMatrix,  n = 50, Title = "Author collaboration \nGISAID",type = "auto", size=10,size.cex=T,edgesize = 3,labelsize=1)
+net = networkPlot(
+  NetMatrix,
+  n = 50,
+  Title = "Author collaboration \nGISAID",
+  type = "auto",
+  size = 10,
+  size.cex = T,
+  edgesize = 3,
+  labelsize = 1
+)
 net2VOSviewer(net, vos.path = "VOSviewer/")
 
 # Education collaboration network
-NetMatrix <- biblioNetwork(M, analysis = "collaboration",  network = "universities", sep = ";")
+NetMatrix <-
+  biblioNetwork(M,
+                analysis = "collaboration",
+                network = "universities",
+                sep = ";")
 summary(networkStat(NetMatrix))
 
 # Main statistics about the network
@@ -95,11 +132,24 @@ summary(networkStat(NetMatrix))
 # Degree Centralization                 0.133
 # Average path length                   3.259
 
-net=networkPlot(NetMatrix,  n = 50, Title = "Institution collaboration\n(GISAID)",type = "auto", size=4,size.cex=F,edgesize = 3,labelsize=1)
+net = networkPlot(
+  NetMatrix,
+  n = 50,
+  Title = "Institution collaboration\n(GISAID)",
+  type = "auto",
+  size = 4,
+  size.cex = F,
+  edgesize = 3,
+  labelsize = 1
+)
 net2VOSviewer(net, vos.path = "VOSviewer/")
 
 # Country collaboration
-NetMatrix <- biblioNetwork(M, analysis = "collaboration",  network = "countries", sep = ";")
+NetMatrix <-
+  biblioNetwork(M,
+                analysis = "collaboration",
+                network = "countries",
+                sep = ";")
 summary(networkStat(NetMatrix))
 # Main statistics about the network
 #
@@ -109,7 +159,17 @@ summary(networkStat(NetMatrix))
 # Diameter                              4
 # Degree Centralization                 0.547
 # Average path length                   1.821
-net=networkPlot(NetMatrix,  n = dim(NetMatrix)[1], Title = "Country collaboration",type = "circle", size=10,size.cex=T,edgesize = 1,labelsize=0.6, cluster="none")
+net = networkPlot(
+  NetMatrix,
+  n = dim(NetMatrix)[1],
+  Title = "Country collaboration",
+  type = "circle",
+  size = 10,
+  size.cex = T,
+  edgesize = 1,
+  labelsize = 0.6,
+  cluster = "none"
+)
 net2VOSviewer(net, vos.path = "VOSviewer/")
 
 
@@ -158,7 +218,7 @@ jpeg("plots/gisaid/topic-fit-gisaid.jpeg",
      height = 800)
 ggplot(plot, aes(K, value, color = variable)) +
   geom_line(size = 1.5, show.legend = FALSE) +
-  facet_wrap( ~ variable, scales = "free_y") +
+  facet_wrap(~ variable, scales = "free_y") +
   labs(x = "Number of topics K",
        title = "Statistical fit of models with different K") +  theme(legend.position =
                                                                         "none",
@@ -256,4 +316,3 @@ for (i in 1:k) {
   )
 }
 dev.off()
-
