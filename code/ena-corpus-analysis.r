44298d422fb03908397a64bdcb9a995ecd28a560
natesheehan <nathanaelsheehan@gmail.com>
2022-12-14 23:01:22 +0000

    update set.up.r file with theme function

--- code/ena-corpus-analysis.r
+++ code/ena-corpus-analysis.r
@@ -218,59 +218,7 @@ ggplot(plot, aes(K, value, color = variable)) +
        title = "Statistical fit of models with different K \nThe Covid-19 Data Portal Corpus") +  theme(legend.position =
                                                                         "none",
                                                                       plot.title = element_text(size = 11)) +
-  theme(
-    legend.position = "none",
-    legend.direction = "horizontal",
-    legend.title = element_text(
-      colour = textcol,
-      face = "italic",
-      size = 14
-    ),
-    legend.margin = margin(grid::unit(0, "cm")),
-    legend.text = element_text(
-      colour = textcol,
-      size = 14,
-      face = "bold"
-    ),
-    legend.key.height = grid::unit(0.8, "cm"),
-    legend.key.width = grid::unit(0.2, "cm"),
-    axis.text.x = element_text(
-      size = 14,
-      angle = 90,
-      vjust = 0.5,
-      hjust = 1,
-      color = textcol
-    ),
-    axis.text.y = element_text(
-      vjust = 0.2,
-      colour = textcol,
-      size = 14
-    ),
-    axis.ticks = element_line(size = 0.4),
-    plot.caption = element_text(colour = textcol, size = 10),
-    axis.title = element_text(
-      size = 12,
-      face = "bold",
-      colour = textcol,
-      hjust = 0.1
-    ),
-    panel.border = element_blank(),
-    panel.background = element_rect(
-      fill = "transparent",
-      colour = "transparent",
-      size = 0.5,
-      linetype = "solid"
-    ),
-    plot.background = element_rect(fill = "gray12"),
-    legend.background = element_rect(fill = "gray12"),
-    plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
-    plot.title = element_text(
-      colour = textcol,
-      size = 18,
-      face = "bold",
-      vjust = 0.9
-    )
-  )
+  theme_k()
 dev.off()
 
 # Set optimal number of K based on fit
--- code/set-up.r
+++ code/set-up.r
@@ -126,6 +126,67 @@ theme_tree = function() {
   )
 }
 
+# K-elbow plot - ggplot2 theme -------------------------------------------
+#' K-elbow plot ggplot2 theme
+#'
+#' @examples
+#' + theme_temporal()
+#'
+theme_k() = function() {
+  theme(
+    legend.position = "none",
+    legend.direction = "horizontal",
+    legend.title = element_text(
+      colour = textcol,
+      face = "italic",
+      size = 14
+    ),
+    legend.margin = margin(grid::unit(0, "cm")),
+    legend.text = element_text(
+      colour = textcol,
+      size = 14,
+      face = "bold"
+    ),
+    legend.key.height = grid::unit(0.8, "cm"),
+    legend.key.width = grid::unit(0.2, "cm"),
+    axis.text.x = element_text(
+      size = 14,
+      angle = 90,
+      vjust = 0.5,
+      hjust = 1,
+      color = textcol
+    ),
+    axis.text.y = element_text(
+      vjust = 0.2,
+      colour = textcol,
+      size = 14
+    ),
+    axis.ticks = element_line(size = 0.4),
+    plot.caption = element_text(colour = textcol, size = 10),
+    axis.title = element_text(
+      size = 12,
+      face = "bold",
+      colour = textcol,
+      hjust = 0.1
+    ),
+    panel.border = element_blank(),
+    panel.background = element_rect(
+      fill = "transparent",
+      colour = "transparent",
+      size = 0.5,
+      linetype = "solid"
+    ),
+    plot.background = element_rect(fill = "gray12"),
+    legend.background = element_rect(fill = "gray12"),
+    plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
+    plot.title = element_text(
+      colour = textcol,
+      size = 18,
+      face = "bold",
+      vjust = 0.9
+    )
+  )
+}
 
 # temporal plot - ggplot2 theme -------------------------------------------
 #' Temporal plot ggplot2 theme
